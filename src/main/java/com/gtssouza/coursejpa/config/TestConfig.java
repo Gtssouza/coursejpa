8c55d0abe2f07756cf7c28c85bc6d149f94c636d
Gustavo Souza <gtssouza94@gmail.com>
2020-04-14 17:36:28 -0300

    Category

--- src/main/java/com/gtssouza/coursejpa/config/TestConfig.java
+++ src/main/java/com/gtssouza/coursejpa/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.gtssouza.coursejpa.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.gtssouza.coursejpa.entities.Category;
+import com.gtssouza.coursejpa.entities.Order;
 import com.gtssouza.coursejpa.entities.User;
+import com.gtssouza.coursejpa.entities.enums.OrderStatus;
+import com.gtssouza.coursejpa.repositories.CategoryRepository;
+import com.gtssouza.coursejpa.repositories.OrderRepository;
 import com.gtssouza.coursejpa.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +22,12 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired /*Injecao de dependencias*/
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override/*Tudo o que colocar dentro desse metodo vai rodar quando a app for iniciada*/
 	public void run(String... args) throws Exception {
@@ -23,7 +35,17 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.DELIVERED,u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID ,u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.SHIPPED, u1);
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		
 	}
 	
---
+++ src/main/java/com/gtssouza/coursejpa/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.gtssouza.coursejpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gtssouza.coursejpa.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+		
+	
+
+}
