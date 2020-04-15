132b904a65a37aa681ce8ba562fac4358ac3719c
Gustavo Souza <gtssouza94@gmail.com>
2020-04-14 22:32:17 -0300

    Inserindo Product

--- src/main/java/com/gtssouza/coursejpa/config/TestConfig.java
+++ src/main/java/com/gtssouza/coursejpa/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.gtssouza.coursejpa.entities.Category;
 import com.gtssouza.coursejpa.entities.Order;
+import com.gtssouza.coursejpa.entities.Product;
 import com.gtssouza.coursejpa.entities.User;
 import com.gtssouza.coursejpa.entities.enums.OrderStatus;
 import com.gtssouza.coursejpa.repositories.CategoryRepository;
 import com.gtssouza.coursejpa.repositories.OrderRepository;
+import com.gtssouza.coursejpa.repositories.ProductRepository;
 import com.gtssouza.coursejpa.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override/*Tudo o que colocar dentro desse metodo vai rodar quando a app for iniciada*/
 	public void run(String... args) throws Exception {
@@ -43,9 +48,16 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers"); 
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 	}
 	
