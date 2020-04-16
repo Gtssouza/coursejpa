8f587ae8be2cd6aff1eabf0ce1c9d4848f188d1f
Gustavo Souza <gtssouza94@gmail.com>
2020-04-16 14:23:29 -0300

    tratamento de excecao delete

--- src/main/java/com/gtssouza/coursejpa/services/UserService.java
+++ src/main/java/com/gtssouza/coursejpa/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gtssouza.coursejpa.entities.User;
 import com.gtssouza.coursejpa.repositories.UserRepository;
+import com.gtssouza.coursejpa.services.exceptions.DatabaseException;
 import com.gtssouza.coursejpa.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
     }
     
     public void delete(Long id) {
+    	try {
     	repository.deleteById(id);
+    	}catch(EmptyResultDataAccessException e) {
+    		throw new ResourceNotFoundException(id);
+    	}catch(DataIntegrityViolationException e) {
+    		throw new DatabaseException(e.getMessage());
+    	}
     }
     
     /*Atualiza Usuario*/
